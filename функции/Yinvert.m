9877407a8001ba1ec353b31f95500f6494a1e615
Zubareva Natalia <ndzubareva@edu.hse.ru>
2020-05-07 16:28:57 +0300

    Добавлен текст программы

--- функции/Yinvert.m
+++ функции/Yinvert.m
@@ -1,17 +1,7 @@
-function Ri = Yinvert(Y,N,M,mu);
-%M = 6;
-%N = 3;
-%Y = fix(100*rand(N,M))
- 
-%include "matrix.h"
-%R = mxCreateNumericMatrix(M, N, mxCOMPLEX_CLASS,1);
- 
- 
+function Ri = Yinvert(Y,N,M,mu)
 %R = mu*eye(N,N) + Y*(Y')
 Ri = eye(M,M)./mu;
 for j = 1:N
-   
     Ry = Ri*Y(:,j);
     Ri = Ri - ( Ry*Ry' )./( 1 + Y(:,j)'*Ry );
 end
-%Ri
--- функции/getPhase.m
+++ функции/getPhase.m
@@ -1,7 +1,4 @@
 function F=getPhase(u,v,Mu,Mv,Um,Vm)
 [p q]=ind2sub([Mu Mv],1:Mu*Mv);
-
-
-
 F=2*j*pi*[p' q']*[u;v];
 end
